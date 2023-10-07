9b0586e8161fb708aa010372e6a47ed9ba47bee5
Maxwell Schwartz <maxwell.schwartz11@gmail.com>
2023-10-06 23:11:06 -0400

    Add game state column

--- app/models.py
+++ app/models.py
@@ -1,4 +1,7 @@
+import enum
+
 from flask_login import UserMixin
+from sqlalchemy import Enum
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db, login
@@ -9,6 +12,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    current_game = db.Column(db.String(64))
 
     def __repr__(self):
         return f"<User {self.username}>"
@@ -20,11 +24,19 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
 
+class GameStateEnum(enum.Enum):
+    waiting_for_word = "Waiting For Word"
+    waiting_for_defs = "Waiting For Definitions"
+    waiting_for_votes = "Waiting For Votes"
+    results = "Results"
+
+
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     game_master = db.Column(db.Integer, db.ForeignKey("user.id"))
     game_code = db.Column(db.String(64), index=True, unique=True)
     round_number = db.Column(db.Integer)
+    game_state = db.Column(Enum(GameStateEnum))
 
     def __repr__(self):
         return f"<Game {self.game_code}>"
